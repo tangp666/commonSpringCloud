02d486de2fe186483321f4055215871a1f3726a4
tangp666 <137889800@qq.com>
2020-10-20 15:37:39 +0800

    eureka 服务

---
+++ eureka/src/main/java/com/pan/eureka/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.pan.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka服务
+ *
+ * @author tangpan
+ * @date 2020/10/20 15:36
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
