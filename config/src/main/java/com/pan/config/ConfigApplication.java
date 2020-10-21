0cad8bd97169cb18bf50faf0f6d0e5815e6acfd1
tangp666 <137889800@qq.com>
2020-10-21 15:31:42 +0800

    1、添加配置中心 2、web项目文件配置修改

---
+++ config/src/main/java/com/pan/config/ConfigApplication.java
@@ -0,0 +1,23 @@
+package com.pan.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 配置中心
+ *
+ * @author tangpan
+ * @date 2020/10/21 13:49
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
